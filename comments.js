d5e0e630f5a2eb9c422d5c2c936755b2feaca3ee
ichigogonzalez <150075499+ichigogonzalez@users.noreply.github.com>
2023-11-20 16:50:21 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,53 +1,40 @@
-// Create Web server application
-
-// Load modules
+// Create web server
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
+
+// Create express app
 const app = express();
 
-// Define port number
-const PORT = process.env.PORT || 3000;
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
 
-// Set views directory
-app.set('views', './views');
+// Create comments object
+const commentsByPostId = {};
 
-// Set view engine
-app.set('view engine', 'ejs');
-
-// Set body-parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
-
-// Set static middleware
-app.use('/public', express.static('public'));
-
-// Create a new comment
-app.post('/comments', (req, res) => {
-    // Get the comment from the form
-    let comment = req.body.comment;
-    // Read the comments from the file
-    let comments = fs.readFileSync('./data/comments.json', 'utf8');
-    // Convert the comments to an array
-    comments = JSON.parse(comments);
-    // Add the new comment to the array
-    comments.push(comment);
-    // Save the comments back to the file
-    fs.writeFileSync('./data/comments.json', JSON.stringify(comments));
-    // Redirect to the comments page
-    res.redirect('/comments');
+// Get comments by post id
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
 });
 
-// Display all comments
-app.get('/comments', (req, res) => {
-    // Read the comments from the file
-    let comments = fs.readFileSync('./data/comments.json', 'utf8');
-    // Convert the comments to an array
-    comments = JSON.parse(comments);
-    // Render the comments page
-    res.render('comments', {comments: comments});
+// Create comment by post id
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    // Get comments by post id
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: commentId, content });
+
+    // Update comments
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Listen on port 4001
+app.listen(4001, () => {
+    console.log('Comments service listening on port 4001');
 });
